c690ca1d63a2882aa45ec7cd38c8480877020941
judgesupreme <1234surajsinghnegi@gmail.com>
2020-08-19 16:51:17 +0530

    Updates

---
+++ precache-manifest.e1acc71630be2c9e0d5c75e34f5c97a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56370c731e8662dd3d6cc61d4772eff6",
+    "url": "/Covid-Tracker/index.html"
+  },
+  {
+    "revision": "1cf74763400d996fddb9",
+    "url": "/Covid-Tracker/static/css/main.5783bf7e.chunk.css"
+  },
+  {
+    "revision": "9aeacca7498ed6ab838d",
+    "url": "/Covid-Tracker/static/js/2.4a154836.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid-Tracker/static/js/2.4a154836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cf74763400d996fddb9",
+    "url": "/Covid-Tracker/static/js/main.2127e563.chunk.js"
+  },
+  {
+    "revision": "d0e8e8f12ec12df5de2e",
+    "url": "/Covid-Tracker/static/js/runtime-main.3cdb2ce1.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-Tracker/static/media/image.d7265326.png"
+  }
+]);
